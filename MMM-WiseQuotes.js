b259430070d921bc8b1c908ea93755b0328fa2c7
Bijay Maharjan - MagicMirror <bijay.maharjan5@gmail.com>
2021-08-29 21:57:13 -0500

    Get data from server

--- MMM-WiseQuotes.js
+++ MMM-WiseQuotes.js
@@ -1,22 +1,12 @@
-let data = {
-    "quotes": [
-        "When love is real, it finds a way.",
-        "They will ignore you, until they need you.",
-        "The less you care, the happier you will be.",
-        "Who gossips to you, will gossip about you.",
-        "Tomorrow never comes, it is always today.",
-        "Wherever life plants you, bloom with grace.",
-        "The best revenge is not to be like your enemy.",
-        "You only need someone to accept you completely.",
-        "There is no path to Happiness: Happiness is the path.",
-        "Work out your own salvation. Don't depend on others.",
-        "Only you can change your life. No one can do it for you.",
-        "Stop looking for happiness in the same place you lost it.",
-        "Sometimes you just have to let go and see what happens.",
-        "True love and Real friends are two of the hardest things to find.",
-        "Make sure you love yourself before you start to love someone else."
-    ]
-}
+/**
+ * MMM Wise Quotes
+ */
+
+// let data = {
+//    "quotes": [
+//        "When love is real, it finds a way."
+//    ]
+//}
 
 Module.register("MMM-WiseQuotes", {
     defaults: {
@@ -55,10 +45,11 @@ Module.register("MMM-WiseQuotes", {
         /**
          * This was used for hard-coded data. No longer used now.
          */
-        // let totalLength = data.quotes.length;
-        // let rand = Math.floor((Math.random() * totalLength));
-        // this.quote = data.quotes[rand];
-        // this.updateDom();
+         // let totalLength = data.quotes.length;
+         // let rand = Math.floor((Math.random() * totalLength));
+         // this.quote = data.quotes[rand];
+         // this.updateDom();
+
 
         fetch(`http://quotes-server.ddns.net:3001/api/v1/quotes/random`).then((response) => {
             response.json().then((quote) => {
@@ -66,6 +57,5 @@ Module.register("MMM-WiseQuotes", {
                 this.updateDom();
             });
         });
-
     }
 });
